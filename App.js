8cfa69a68a5f66ab35cc987d9cd3506881d7891d
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-09-29 17:14:14 -0300

    conclui pt 6: using the state hook

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {Button, Linking, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App: () => Node = () => {
+  const [name, setName] = useState('Anderson');
+  const [session, setSession] = useState({number: 6, title: 'State'});
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName('Anderson Vieira');
+    setSession({number: 7, title: 'Style'});
+    setCurrent(false);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Teste qualquer</Text>
-      <Button
-        title={'Botão que abre o tutorial no youtube'}
-        onPress={() => {
-          Linking.openURL('https://youtu.be/ANdSdIlgsEw');
-        }}
-      />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>
+        Esta é a sessão de número {session.number} e é sobre {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'sessão atual' : 'próxima sessão'}
+      </Text>
+      <Button title={'Atualiza State'} onPress={onClickHandler} />
     </View>
   );
 };
