7cc3d55373eb474bc7ed70827c9f0fa28ab3e640
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-10-02 07:44:48 -0300

    conclui pt 08: responsive ui with flexbox

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App: () => Node = () => {
-  const [name, setName] = useState('Style Test');
-
-  const onClickHandler = () => {
-    setName('Style Test is Done!');
-  };
-
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <View style={styles.button}>
-        <Button title={'Atualiza State'} onPress={onClickHandler} />
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
       </View>
     </View>
   );
@@ -20,19 +19,34 @@ const App: () => Node = () => {
 
 const styles = StyleSheet.create({
   body: {
-    width: '100%',
-    height: '50%',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
     backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: '#ff00ff',
-    borderRadius: 10,
   },
   text: {
     color: '#000000',
+    fontSize: 35,
     fontStyle: 'italic',
-    fontSize: 40,
     margin: 10,
     textTransform: 'uppercase',
   },
