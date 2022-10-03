91ab5b39b4134dd3b689d9496a750f2a0d3a13b0
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-10-03 18:09:47 -0300

    responde exercicio pt 08: responsive ui with flexbox'

--- App.js
+++ App.js
@@ -4,14 +4,34 @@ import {StyleSheet, Text, View} from 'react-native';
 const App: () => Node = () => {
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
+      <View style={styles.container1}>
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
       </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
+      <View style={styles.container1}>
+        <View style={styles.view4}>
+          <Text style={styles.text}>4</Text>
+        </View>
       </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
+      <View style={styles.container1}>
+        <View style={styles.view5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+      <View style={styles.container2}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>
       </View>
     </View>
   );
@@ -21,8 +41,19 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#0000ff',
+    backgroundColor: '#000000',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  container1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 3,
+    flexDirection: 'row',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   view1: {
@@ -32,14 +63,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   view2: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#ff00ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   view3: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view4: {
+    flex: 1,
+    backgroundColor: '#FF0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view5: {
+    flex: 1,
+    backgroundColor: '#00FF00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view6: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view7: {
+    flex: 1,
+    backgroundColor: '#0000FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -49,10 +104,6 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
     margin: 10,
     textTransform: 'uppercase',
-  },
-  button: {
-    width: 200,
-    height: 60,
   },
 });
 
